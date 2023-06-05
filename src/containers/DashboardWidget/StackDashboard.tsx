60419d7987b43adeb23714a6a8be28f1d1668a0f
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-06-05 18:06:48 +0530

    feat: add modal support for dashboard location

--- src/containers/DashboardWidget/StackDashboard.tsx
+++ src/containers/DashboardWidget/StackDashboard.tsx
@@ -1,22 +1,43 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
+import { Button, cbModal } from "@contentstack/venus-components";
+
+import "@contentstack/venus-components/build/main.css";
 import Icon from "../../assets/Icon.svg";
 import localeTexts from "../../common/locales/en-us/index";
 import parse from "html-react-parser";
 import { MarketplaceAppContext } from "../../common/contexts/marketplaceContext";
+import SelectModal from "./SelectModal";
+import "./styles.css";
 
 const StackDashboardExtension = () => {
   const appSDK = useContext(MarketplaceAppContext).appSdk;
+  const ref = useRef(null);
 
   useEffect(() => {
     appSDK?.pulse("Stack Dashboard UI Location loaded", { appUid: appSDK.appUID });
+    const iframeWrapperRef = ref.current;
+    (window as any).iframeRef = iframeWrapperRef;
+    (window as any).postRobot = appSDK.postRobot;
   }, []);
 
+  const handleClick = (e) => {
+    cbModal({
+      component: (props) => <SelectModal {...props} />,
+      modalProps: {
+        size: "max",
+      },
+    });
+  };
+
   return (
-    <div className="dashboard">
+    <div ref={ref} className="dashboard">
       <div className="dashboard-container">
         <div className="dashboard-icon">
           <img src={Icon} alt="icon" />
         </div>
+        <Button buttonType="tertiary-outline" onClick={handleClick}>
+          Open Modal
+        </Button>
         <div className="app-component-content">
           <h4>{localeTexts.DashboardWidget.title}</h4>
           <p>{parse(localeTexts.DashboardWidget.body)}</p>
