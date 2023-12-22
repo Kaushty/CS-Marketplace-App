7d9f940d95f912f2fc3a70fcb55ab14fff528c5e
Kaushik Shetty <kaushik.shetty@contentstack.com>
2023-12-22 13:55:07 +0530

    fix: add modal with select to app config page

--- src/containers/ConfigScreen/AppConfiguration.tsx
+++ src/containers/ConfigScreen/AppConfiguration.tsx
@@ -4,6 +4,7 @@ import { useContext, useEffect, useRef, useState } from "react";
 import Icon from "../../assets/appconfig.svg";
 import { MarketplaceAppContext } from "../../common/contexts/marketplaceContext";
 import localeTexts from "../../common/locales/en-us/index";
+import { FieldLabel, InstructionText, Select, openUploadAssetModal } from "@contentstack/venus-components";
 
 const AppConfigurationExtension = () => {
   const appSDK = useContext(MarketplaceAppContext).appSdk;
@@ -23,6 +24,59 @@ const AppConfigurationExtension = () => {
     asyncOp();
   }, []);
 
+  const handleClick = (e) => {
+    console.log("APP CLICK", Date.now());
+    openUploadAssetModal({
+      sdk: appSDK,
+      onSubmit: (e) => {
+        console.log("KS APP: Upload", e);
+      },
+    });
+  };
+
+  const data = [
+    {
+      label: "Group 1",
+      options: [
+        { label: "item 1", value: "value_1" },
+        { label: "item 2", value: "value_2" },
+        { label: "item 3", value: "value_3" },
+        { label: "item 4", value: "value_4" },
+      ],
+    },
+    {
+      label: "Group 2",
+      options: [
+        { label: "item 5", value: "value_5" },
+        { label: "item 6", value: "value_6" },
+        { label: "item 7", value: "value_7" },
+        { label: "item 8", value: "value_8" },
+      ],
+    },
+    {
+      label: "Group 3",
+      options: [
+        { label: "item 9", value: "value_9" },
+        { label: "item 10", value: "value_10" },
+        { label: "item 11", value: "value_11" },
+        { label: "item 12", value: "value_12" },
+      ],
+    },
+    {
+      label: "Group 4",
+      options: [
+        { label: "item 13", value: "value_13" },
+        { label: "item 14", value: "value_14" },
+        { label: "item 15", value: "value_15" },
+        { label: "item 16", value: "value_16" },
+      ],
+    },
+    { label: "root option 1", value: "value_root_1" },
+    { label: "root option 2", value: "value_root_2" },
+  ];
+
+  const [value, updateValue] = useState(null);
+
   return (
     <div className="app-config">
       <div className="app-config-container">
@@ -39,13 +93,25 @@ const AppConfigurationExtension = () => {
             {localeTexts.ConfigScreen.button.learnMore}
           </a>
         </div>
-        <button
-          onClick={() => {
-            appConfig.current?.installation.setValidity(!isValid);
-            setIsValid((isValid) => !isValid);
-          }}>
-          Toggle Save
-        </button>
+        <button onClick={handleClick}>Toggle Save</button>
+        <div>
+          <FieldLabel htmlFor="select">
+            <InstructionText>Select Something</InstructionText>
+            <Select
+              selectLabel="Grouped Select Options"
+              value={value}
+              isMulti={true}
+              onChange={updateValue}
+              options={data}
+              placeholder="Select Title"
+              isSearchable={true}
+              isClearable={true}
+              width="400px"
+              multiDisplayLimit={2}
+              version="v2"
+            />
+          </FieldLabel>
+        </div>
       </div>
     </div>
   );
